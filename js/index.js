265a6ed566cc2583312d638244e30771c496278b
Sean Doyle <sdoylelambda@gmail.com>
2019-03-05 18:25:51 -0500

    Yllow on button press, blue on copy.

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@
 // const workBtn = document.getElementsByClassName('btn');
 // workBtn.addEventListener('mouseover', function(eventObject){
 //  console.log(`btn fired!`);
-//   TweenMax.to("#btn", 3, {x:100, y:100, scale:2, skewX:45, rotation:180});
+//   TweenMax.to("#btn", 1.5, {width:0, height:0}, {width:100, height:200})
 // });
-
 
 // Red on click
 const container = document.querySelector('html');
@@ -19,7 +18,6 @@ container.addEventListener('click', function(eventObject){
 // Black on dblClick
 const containerBack = document.querySelector('html');
 containerBack.addEventListener('dblclick', function(eventObject){
- //console.log(`Event Target: ${eventObject.target}`);
  console.log(`Current Target: ${eventObject.currentTarget}`);
   eventObject.target.style.color = "black";
   //eventObject.currentTarget.style.color = "red";
@@ -31,6 +29,25 @@ greenMouse.addEventListener('contextmenu', function(eventObject){
     console.log(`Hovering Target: ${eventObject.currentTarget}`);
     eventObject.target.style.color = "green";
 });
+
+// Blue on copy
+const copyBlue = document.querySelector('html');
+copyBlue.addEventListener('copy', function(eventObject){
+ console.log(`Current Target: ${eventObject.currentTarget}`);
+  eventObject.target.style.color = "blue";
+});
+
+
+// Yellow on key presss
+const purp = document.querySelector('html');
+purp.addEventListener('keydown', function(eventObject){
+ //console.log(`Event Target: ${eventObject.target}`);
+ console.log(`Current Target: ${eventObject.currentTarget}`);
+//   eventObject.target.style.color = "green";
+  eventObject.currentTarget.style.color = "yellow";
+});
+
+
 
 // 
 // const keyTrack = document.querySelector('html');
